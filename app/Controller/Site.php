11f2b3252f2066879452f451bfe034e02519b2b3
Vris111 <ruslansimanovic56@mail.ru>
2024-03-25 15:38:41 +0700

    Create files for structure

---
+++ app/Controller/Site.php
@@ -0,0 +1,15 @@
+<?php
+namespace Controller;
+
+class Site
+{
+    public function index(): void
+    {
+        echo 'wi';
+    }
+
+    public function hello(): void
+    {
+        echo 'wi';
+    }
+}
